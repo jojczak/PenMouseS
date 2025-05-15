9307e87677f8b7bd2342392866e4c0616f282181
Jojczak <kubjan4@gmail.com>
2025-05-15 15:32:39 +0200

    Bump version to 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "pl.jojczak.penmouses"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.9.1"
+        versionCode = 3
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
