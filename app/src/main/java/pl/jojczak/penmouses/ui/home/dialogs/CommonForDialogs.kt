39c73936992c23d959db2a54311dadc917b21aee
Jojczak <kubjan4@gmail.com>
2025-05-15 15:23:20 +0200

    The text color in WebView dialogs now matches the theme color. Added a link to the instructional video on YouTube.

--- app/src/main/java/pl/jojczak/penmouses/ui/home/dialogs/CommonForDialogs.kt
+++ app/src/main/java/pl/jojczak/penmouses/ui/home/dialogs/CommonForDialogs.kt
@@ -1,9 +1,13 @@
 package pl.jojczak.penmouses.ui.home.dialogs
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Color
 import android.net.Uri
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.annotation.OptIn
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
@@ -11,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -20,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,13 +38,38 @@ import pl.jojczak.penmouses.ui.theme.PLAYER_HEIGHT
 import pl.jojczak.penmouses.ui.theme.PenMouseSTheme
 import pl.jojczak.penmouses.ui.theme.radius_l
 
+@SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebView(url: String) {
+    val textColor = MaterialTheme.colorScheme.onBackground
+    val textColorHex = String.format("#%06X", 0xFFFFFF and textColor.toArgb())
+
     AndroidView(
         factory = { context ->
             WebView(context).apply {
-                loadUrl(url)
+                settings.javaScriptEnabled = true
                 setBackgroundColor(Color.TRANSPARENT)
+
+                webViewClient = object : WebViewClient() {
+                    override fun shouldOverrideUrlLoading(
+                        view: WebView?,
+                        request: WebResourceRequest
+                    ): Boolean {
+                        val intent = Intent(Intent.ACTION_VIEW, request.url)
+                        context.startActivity(intent)
+                        return true
+                    }
+
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+
+                        val js = """
+                        document.body.style.color = '$textColorHex';
+                    """.trimIndent()
+                        view?.evaluateJavascript(js, null)
+                    }
+                }
+                loadUrl(url)
             }
         }
     )
